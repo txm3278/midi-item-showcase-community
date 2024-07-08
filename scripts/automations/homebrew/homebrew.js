f8229ff7de8e98ed6b34d535746f560f1a430ab3
Tom Mineo <tmineo98@gmail.com>
2024-07-08 18:38:00 -0400

    fix incorrect path

--- scripts/automations/homebrew/homebrew.js
+++ scripts/automations/homebrew/homebrew.js
@@ -1,7 +1,7 @@
 import { allOrNothingArmor } from './items/allOrNothingArmor.js';
 import { potionOfHealing } from './items/potionOfHealing.js';
 import { tomb } from './TOMB/tomb.js';
-import { fleeMortals } from './Flee, Mortals!/fleeMortals.js';
+import { fleeMortals } from './FleeMortals/fleeMortals.js';
 
 export let homebrew = {
   allOrNothingArmor,
