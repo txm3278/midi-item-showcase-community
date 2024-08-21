401dd79366d863290f07a034f1ea6e61f3618b3c
Alexander James <aljames.arctic@gmail.com>
2024-08-20 23:33:15 -0500

    Energy Shield

--- scripts/automations/homebrew/items/Energy Shield.js
+++ scripts/automations/homebrew/items/Energy Shield.js
@@ -8,35 +8,32 @@ async function effectEach() {
 }
 
 async function isDamaged() {
-    const damageArray = workflow.damageItem.damageDetail.flat(1).filter(d=>d);
-    const piercing = damageArray.partition(d=>d.type !== 'piercing')[0].reduce((psum,a) => psum + a.damage, 0);
-    const slashing = damageArray.partition(d=>d.type !== 'bludgeoning')[0].reduce((psum,a) => psum + a.damage, 0);
-    const bludgeoning = damageArray.partition(d=>d.type !== 'slashing')[0].reduce((psum,a) => psum + a.damage, 0);
-    const shieldPhysicalDamage = piercing + slashing + bludgeoning;
-    console.warn("A total of", shieldPhysicalDamage, "physical damage");  // verified working
+    let remainingDamage = workflow.damageItem.hpDamage;
+
+    const bludgeoning = workflow.damageDetail.find(d=>d.type=='bludgeoning')?.damage ?? 0;
+    const slashing = workflow.damageDetail.find(d=>d.type=='slashing')?.damage ?? 0;
+    const piercing = workflow.damageDetail.find(d=>d.type=='piercing')?.damage ?? 0;
+    const healing = workflow.damageDetail.find(d=>d.type=='healing')?.damage ?? 0;
+
+    const physicalDamage = piercing + slashing + bludgeoning;
+    const energyDamage = remainingDamage - physicalDamage - healing;
 
     const { value, max } = actor.system.resources.tertiary ?? {};
-    const shieldEnergyStrength = value;
-    if (!shieldEnergyStrength) return console.warn(`${actor.name} shield is depleted`);
-    //Calc Phys vs Nonphys
-    console.warn("Original damage is", workflow.damageItem.hpDamage);
-    let remainingDamage = workflow.damageItem.hpDamage;
-    const shieldEnergyDamage = remainingDamage - shieldPhysicalDamage;
+    let remainingShield = value;
+    if (!remainingShield) return console.warn(`${actor.name} shield is depleted`);
 
     // non-phys trades 1 for 1
-    let shieldRemaining = Math.max(shieldEnergyStrength - shieldEnergyDamage, 0);
-    console.warn("non-physical damage reduced shields from", shieldEnergyStrength, "to", shieldRemaining);
-    remainingDamage -= (shieldEnergyStrength - shieldRemaining);
+    let shieldDamage = Math.min(energyDamage, remainingShield);
+    remainingDamage -= shieldDamage;
+    remainingShield -= shieldDamage;
   
     // phys trades at 2 for 1
-    const shieldPhysicalStrength = Math.ceil(shieldRemaining / 2);
-    shieldRemaining = 2 * Math.max(shieldPhysicalStrength - shieldPhysicalDamage, 0);
-    console.warn("physical damage reduced shields from", 2*shieldPhysicalStrength, "+/- 1 to", shieldRemaining);
-    remainingDamage -= (shieldPhysicalStrength - shieldRemaining / 2);
+    shieldDamage = Math.min(physicalDamage * 2, remainingShield);
+    remainingDamage -= Math.ceil(shieldDamage / 2);
+    remainingShield -= shieldDamage;
   
-    console.log("Overflow damage, after everything is", remainingDamage);
     workflow.damageItem.hpDamage = remainingDamage;
-    await actor.update({ 'system.resources.tertiary.value': shieldRemaining});
+    await actor.update({ 'system.resources.tertiary.value': remainingShield});
 }
 
 async function onEffect() {}
