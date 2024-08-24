7dca482e18031be41fae45863b39a9a40f0ef00e
bakanabaka <3474820+aljames-arctic@users.noreply.github.com>
2024-08-23 21:51:45 -0500

    Update Dagger of Venom - Black Poison.js

--- scripts/automations/items/Dagger of Venom - Black Poison.js
+++ scripts/automations/items/Dagger of Venom - Black Poison.js
@@ -1,18 +1,9 @@
 // @bakanabaka
 
-async function postActiveEffects() {
-    function findItem(itemName) {
-        return actor.items.find(it => it.name == itemName); 
-    }
-
-    let dagger = findItem("Dagger of Venom");
-    if (!dagger) return;
-
-    // Add poison effect
-    let enableEffect = dagger.effects.find(ef => ef.name.includes("Enabled"));
-    await enableEffect.update({"disabled": false});
+let dagger = actor.items.find(it => it.name == "Dagger of Venom"); 
+if (!dagger) {
+    console.error("'Dagger of Venom' not found on actor!");
+    return;
 }
-
-await macroUtil.runWorkflows(arguments, {
-    postActiveEffects : postActiveEffects
-});
+let enableEffect = dagger.effects.find(ef => ef.name.includes(macroItem.name));
+await enableEffect.update({"disabled": false});
