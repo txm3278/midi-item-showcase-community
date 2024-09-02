bbb4c33a53db4115ba62b08311e1735e5bc03e5f
aljames-arctic <bakanabaka.vtt@gmail.com>
2024-09-02 15:14:52 -0500

    Update Black Poison search critereon

--- scripts/automations/items/Dagger of Venom - Black Poison.js
+++ scripts/automations/items/Dagger of Venom - Black Poison.js
@@ -3,7 +3,7 @@
 async function postActiveEffects() {
     let dagger = actor.items.find(it => it.name == "Dagger of Venom"); 
     if (!dagger) return;
-    let enableEffect = dagger.effects.find(ef => ef.name.includes(macroItem.name));
+    let enableEffect = dagger.effects.find(ef => ef.name == `${macroItem.name} Applied`);
     await enableEffect.update({"disabled": false});
 }
 
