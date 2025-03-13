562a3f8ac1384a2a1058371f3b3c1ba7718ac308
pgervais70 <patrice_gervais@yahoo.com>
2025-03-12 21:59:37 -0400

    Add missing wardingManeuver amd remove duplicate Steady Aim

--- scripts/automations/classFeatures/classFeatures.js
+++ scripts/automations/classFeatures/classFeatures.js
@@ -1,29 +1,31 @@
+import { flashOfGenius } from './Artificer/flashOfGenius.js';
 import { spiritShield } from './Barbarian/PathOfTheAncestrialGuardian/spiritShield.js';
 import { vengefulAncestors } from './Barbarian/PathOfTheAncestrialGuardian/vengefulAncestors.js';
-import { sentinelAtDeathsDoor } from './Cleric/GraveDomain/sentinelAtDeathsDoor.js';
-import { unwaveringMark } from './Fighter/Cavalier/unwaveringMark.js';
-import { psionicPowerProtectiveField } from './Fighter/PsiWarrior/psionicPowerProtectiveField.js';
-import { divineAllegiance } from './Paladin/OathOfTheCrown/divineAllegiance.js';
-import { wardingFlare } from './Cleric/LightDomain/wardingFlare.js';
-import { flashOfGenius } from './Artificer/flashOfGenius.js';
-import { channelDivinityVowOfEnmity } from './Paladin/OathOfVengeance/channelDivinityVowOfEnmity.js';
 import { sorrowfulFate } from './Bard/CollegeOfTragedy/sorrowfulFate.js';
+import { sentinelAtDeathsDoor } from './Cleric/GraveDomain/sentinelAtDeathsDoor.js';
+import { wardingFlare } from './Cleric/LightDomain/wardingFlare.js';
 import { dampenElements } from './Cleric/NatureDomain/dampenElements.js';
+import { unwaveringMark } from './Fighter/Cavalier/unwaveringMark.js';
+import { wardingManeuver } from './Fighter/Cavalier/wardingManeuver.js';
+import { psionicPowerProtectiveField } from './Fighter/PsiWarrior/psionicPowerProtectiveField.js';
 import { channelDivinityRebukeTheViolent } from './Paladin/OathOfRedemption/channelDivinityRebukeTheViolent.js';
+import { divineAllegiance } from './Paladin/OathOfTheCrown/divineAllegiance.js';
+import { channelDivinityVowOfEnmity } from './Paladin/OathOfVengeance/channelDivinityVowOfEnmity.js';
 import { steadyAim } from './Rogue/steadyAim.js';
 
 export let classFeatures = {
+  flashOfGenius,
   spiritShield,
   vengefulAncestors,
-  sentinelAtDeathsDoor,
-  unwaveringMark,
-  psionicPowerProtectiveField,
-  divineAllegiance,
-  wardingFlare,
-  flashOfGenius,
-  channelDivinityVowOfEnmity,
   sorrowfulFate,
+  sentinelAtDeathsDoor,
+  wardingFlare,
   dampenElements,
+  unwaveringMark,
+  wardingManeuver,
+  psionicPowerProtectiveField,
   channelDivinityRebukeTheViolent,
+  divineAllegiance,
+  channelDivinityVowOfEnmity,
   steadyAim,
 };
